5d10f436c0e35ddd2028e338f157dcc8234451cf
cwu155 <cwu17@stuy.edu>
2019-02-01 01:36:49 -0500

    Added sqrt and updateGuess

--- recursion.java
+++ recursion.java
@@ -1,14 +1,28 @@
 public class recursion{
-    /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
      *tolerance is the allowed percent error the squared answer is away from n.
      *precondition: n is non-negative
-
     */
-    // public static double sqrt(double n, double tolerance){
-    //
-    // }
+
+    //A lot of this code looks super unnecessary!!!
+
+    public static double sqrt(double n, double tolerance){
+      double guess = n/2;
+        if (n < 1){
+          return 1;
+        } else {
+          return updateGuess(guess, n, tolerance);
+        }
+      }
+
+    private static double updateGuess(double guess, double n, double tolerance){
+      double test = guess * guess;
+      if (((Math.abs(test - n)) / n) > tolerance){
+        guess = updateGuess(((n / guess + guess) / 2), n, tolerance);
+      }
+      return guess;
+    }
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
@@ -21,5 +35,12 @@ public class recursion{
     /*As Per classwork*/
     // public static ArrayList<Integer> makeAllSums(){
     // }
+
+  //Testing.
+  public static void main(String[] args) {
+    System.out.println(sqrt(100.0, 0.001));
+    System.out.println(sqrt(9, 0.001));
+    System.out.println(sqrt(1, 0.001));
+  }
 
 }
